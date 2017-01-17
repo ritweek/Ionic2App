32039f88264f27861c006f4a2883a12293911f54
Ritweek Ranjan <rranjan@BLR.ALLEGISINDIA.COM>
2017-01-17 16:26:28 +0530

    Second Commit

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -15,7 +15,7 @@ export class CameraPage {
     console.log('Hello From Camera Page');
   }
 
-  photos: Photo[] = [new Photo('https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAOtAAAAJGM3ZDBhODkxLTg1MTMtNGQ1Zi05NjVhLTUzNDkzOTllZTYyYg.jpg', 7), new Photo('http://placehold.it/350/151', 8)]
+  photos: Photo[] = [new Photo('https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAOtAAAAJGM3ZDBhODkxLTg1MTMtNGQ1Zi05NjVhLTUzNDkzOTllZTYyYg.jpg', 7), new Photo('https://d13yacurqjgara.cloudfront.net/users/729829/screenshots/3177853/galshir-babushka.gif', 8)]
 
 takePhoto(){
   Camera.getPicture({
@@ -53,5 +53,20 @@ removePhoto(photo){
 
 likePhoto(photo){
   photo.likes++;
+}
+
+getGalleryPicture(){
+  Camera.getPicture({
+    destinationType: Camera.DestinationType.DATA_URL,
+    sourceType:Camera.PictureSourceType.PHOTOLIBRARY,
+    targetHeight: 500,
+    targetWidth: 500,
+    correctOrientation: true    
+  }).then((imageData)=>{
+  this.photos.push(new Photo("data:image/jpeg;base64," + imageData, 0));
+},
+(err)=>{
+  console.log(err);
+  });
 }
 }
